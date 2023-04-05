5d41bd2fc1dbe05559cabc6fcac1935f2621e498
Andrew Diamond <amdiamond107@gmail.com>
2023-04-05 15:58:36 -0400

    done

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
+const input = document.getElementById('button');
+
 function addingEventListener() {
+    alert('I was clicked');
+
+input.addEventListener('click', clickAlert);
+}
+
+function clickAlert() {
+    alert('I was clicked');
 }
